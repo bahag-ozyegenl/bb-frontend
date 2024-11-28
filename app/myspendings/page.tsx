84d49213ac2cdf0040e4f8fb3c40ac95a841c36b
ozyegenl <lara.ozyegen@bahag.com>
2024-11-28 20:27:15 +0100

    black background fixed pie chart added

--- app/myspendings/page.tsx
+++ app/myspendings/page.tsx
@@ -16,13 +16,20 @@ const MySpendings = () => {
   const [error, setError] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [editingId, setEditingId] = useState<number | null>(null);
+  const [editedSpending, setEditedSpending] = useState<Spending | null>(null);
 
-  
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [spending, setSpending] = useState({ //this is used to add a new spending
+    name: '',
+    amount: '',
+    date: '',
+    category: '',
+  });
 
-  // Fetch all spendings on page load
-  useEffect(() => {
-    fetchSpendings();
-  }, []);
+
+    useEffect(() => {
+        fetchSpendings();
+    }, []);
 
   const fetchSpendings = async () => {
     try {
@@ -33,7 +40,7 @@ const MySpendings = () => {
           Authorization: `Bearer ${token}`,
         },
       });
-      console.log(response)
+
       if (!response.ok) {
         throw new Error('Failed to fetch spendings');
       }
@@ -71,52 +78,178 @@ const MySpendings = () => {
     }
   };
 
-  const handleInputChange = (id: number, e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    console.log("e val ", e.target.value)
-    const { name, value } = e.target;
-    setSpendings((prevSpendings) =>
-    prevSpendings.map((spending: any) =>
-    spending.id === id
-     ? { ...spending, [name]: value } // Update the specific spending being edited
-     : spending
-   )
-  );
-};
+  const handleInputChangeAddSpending = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setSpending({ ...spending, [e.target.name]: e.target.value });
+    
+  };
 
-  const handleSave = async (id: number) => {
-    // console.log("edited spending saved: ", spending);
-    const spendingToSave = spendings.find((spending) => spending.id === id);
-    setIsModalOpen(false);
+  const handleSaveAddSpending = async () => {
+    console.log("Spending saved: ", spending);
+    setIsAddModalOpen(false);
 
     const token = localStorage.getItem('token');
     try {
-      const res = await fetch(`http://localhost:3000/api/spending/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(spendingToSave),
-      });
+        const res = await fetch(`http://localhost:3000/api/spending`, {
+            method: 'POST',
+            headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(spending),
+        });
 
-      if (!res.ok) throw new Error('Failed to add spending');
+        if (!res.ok) throw new Error('Failed to add spending');
+        const data = await res.json(); // Parse response
+        const newSpending = data.spending; // Extract the spending object
+
+        setSpendings((prevSpendings) => [...prevSpendings, newSpending]); // Update spendings with the new item
       
     } catch (err: any) {
         setError(err.message);
+    }
+
+    
+  };
+
+
+  const openEditModal = (id: number) => {
+    setEditingId(id);
+    const spendingToEdit = spendings.find((spending) => spending.id === id);
+    if (spendingToEdit) {
+      console.log("spendingToEdit ", spendingToEdit)
+      setEditedSpending({ ...spendingToEdit}); // Ensure the date is formatted correctly
+      setIsModalOpen(true);
+    }
+  };
+  
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setEditedSpending((prev) => (prev ? { ...prev, [name]: value } : null));
+  };
+
+  const handleSave = async () => {
+    if (!editedSpending || editingId === null) return;
+
+    const token = localStorage.getItem('token');
+    try {
+      const res = await fetch(`http://localhost:3000/api/spending/${editingId}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedSpending),
+      });
+
+      if (!res.ok) throw new Error('Failed to update spending');
+
+      // Update the state with the saved spending
+      setSpendings((prevSpendings) =>
+        prevSpendings.map((spending) =>
+          spending.id === editingId ? { ...editedSpending } : spending
+        )
+      );
+
+      setIsModalOpen(false);
+    } catch (err: any) {
+      setError(err.message);
     }
   };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
-  const openEditModal = (id: number) => {
-    setEditingId(id); // Set the ID of the spending being edited
-    setIsModalOpen(true);
-   };
-
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-2xl font-bold mb-4">My Spendings</h1>
+       <>
+       <div className="flex items-center justify-between mb-4">
+          <h1 className="text-2xl font-bold mb-4">My Spendings</h1>
+          <button
+            onClick={() => setIsAddModalOpen(true)}
+            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+          >
+            Add Spending
+          </button>
+        </div>
+
+          {isAddModalOpen && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="bg-white p-6 rounded shadow-lg w-96">
+                <h2 className="text-lg font-bold text-gray-700 mb-4">Add Spending</h2>
+                <form className="space-y-4">
+                  <div>
+                    <label className="block text-sm font-medium text-gray-700">Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={spending.name}
+                      onChange={handleInputChangeAddSpending}
+                      className="block w-full border rounded px-3 py-2"
+                      placeholder="Spending name"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-sm font-medium text-gray-700">Amount</label>
+                    <input
+                      type="number"
+                      name="amount"
+                      value={spending.amount}
+                      onChange={handleInputChangeAddSpending}
+                      className="block w-full border rounded px-3 py-2"
+                      placeholder="Spending amount"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-sm font-medium text-gray-700">Date</label>
+                    <input
+                      type="date"
+                      name="date"
+                      value={spending.date}
+                      onChange={handleInputChangeAddSpending}
+                      className="block w-full border rounded px-3 py-2"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-sm font-medium text-gray-700">Category</label>
+                    <select
+                      name="category"
+                      value={spending.category}
+                      onChange={handleInputChangeAddSpending}
+                      className="block w-full border rounded px-3 py-2"
+                      required
+                    >
+                      <option value="" disabled>Select category</option>
+                      <option value="Food">Food</option>
+                      <option value="Transport">Transport</option>
+                      <option value="Entertainment">Entertainment</option>
+                      <option value="Utilities">Utilities</option>
+                      <option value="Other">Other</option>
+                    </select>
+                  </div>
+                </form>
+                <div className="flex justify-end space-x-4 mt-6">
+                  <button
+                    onClick={() => setIsAddModalOpen(false)}
+                    className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleSaveAddSpending}
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+
       <table className="table-auto w-full border-collapse border border-gray-300">
         <thead>
           <tr>
@@ -136,87 +269,11 @@ const MySpendings = () => {
               <td className="border border-gray-300 px-4 py-2">{spending.category}</td>
               <td className="border border-gray-300 px-4 py-2">
                 <button
-                  onClick={() => 
-                    openEditModal(spending.id)}
+                  onClick={() => openEditModal(spending.id)}
                   className="mr-2 text-blue-500 hover:underline"
                 >
                   Edit
                 </button>
-                {isModalOpen && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="bg-white p-6 rounded shadow-lg w-96">
-                        <h2 className="text-lg font-bold text-gray-700 mb-4">Update Spending</h2>
-                        <form className="space-y-4">
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700">Name</label>
-                            <input
-                            type="text"
-                            name="name"
-                            value={spending.name}
-                            onChange={(e) => handleInputChange(spending.id, e)}
-                            className="block w-full border rounded px-3 py-2"
-                            placeholder="Spending name"
-                            required
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700">Amount</label>
-                            <input
-                            type="number"
-                            name="amount"
-                            value={spending.amount}
-                            onChange={(e) => handleInputChange(spending.id, e)}
-                            className="block w-full border rounded px-3 py-2"
-                            placeholder="Spending amount"
-                            required
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700">Date</label>
-                            <input
-                            type="date"
-                            name="date"
-                            value={spending.date}
-                            onChange={(e) => handleInputChange(spending.id, e)}
-                            className="block w-full border rounded px-3 py-2"
-                            required
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-sm font-medium text-gray-700">Category</label>
-                            <select
-                            name="category"
-                            value={spending.category}
-                            onChange={(e) => handleInputChange(spending.id, e)}
-                            className="block w-full border rounded px-3 py-2"
-                            required
-                            >
-                            <option value="" disabled>Select category</option>
-                            <option value="Food">Food</option>
-                            <option value="Transport">Transport</option>
-                            <option value="Entertainment">Entertainment</option>
-                            <option value="Utilities">Utilities</option>
-                            <option value="Other">Other</option>
-                            </select>
-                        </div>
-                        </form>
-                        <div className="flex justify-end space-x-4 mt-6">
-                        <button
-                            onClick={() => setIsModalOpen(false)}
-                            className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            onClick={() => handleSave(spending.id)}
-                            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                        >
-                            Save
-                        </button>
-                        </div>
-                    </div>
-                    </div>
-                )}
                 <button
                   onClick={() => handleDelete(spending.id)}
                   className="text-red-500 hover:underline"
@@ -228,6 +285,82 @@ const MySpendings = () => {
           ))}
         </tbody>
       </table>
+
+      {isModalOpen && editedSpending && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded shadow-lg w-96">
+            <h2 className="text-lg font-bold text-gray-700 mb-4">Update Spending</h2>
+            <form className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={editedSpending.name}
+                  onChange={handleInputChange}
+                  className="block w-full border rounded px-3 py-2"
+                  placeholder="Spending name"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Amount</label>
+                <input
+                  type="number"
+                  name="amount"
+                  value={editedSpending.amount}
+                  onChange={handleInputChange}
+                  className="block w-full border rounded px-3 py-2"
+                  placeholder="Spending amount"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Date</label>
+                <input
+                  type="date"
+                  name="date"
+                  value={editedSpending.date}
+                  onChange={handleInputChange}
+                  className="block w-full border rounded px-3 py-2"
+                  required
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">Category</label>
+                <select
+                  name="category"
+                  value={editedSpending.category}
+                  onChange={handleInputChange}
+                  className="block w-full border rounded px-3 py-2"
+                  required
+                >
+                  <option value="" disabled>Select category</option>
+                  <option value="Food">Food</option>
+                  <option value="Transport">Transport</option>
+                  <option value="Entertainment">Entertainment</option>
+                  <option value="Utilities">Utilities</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
+            </form>
+            <div className="flex justify-end space-x-4 mt-6">
+              <button
+                onClick={() => setIsModalOpen(false)}
+                className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleSave}
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

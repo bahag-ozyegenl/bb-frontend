79a39b15537fd0e13cd9b509b348fb11fb62ef44
buzasg <george.buzas@bahag.com>
2024-12-04 15:06:06 +0100

    modified docker and deployment files + added youtube api

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -53,6 +53,13 @@ export default function Example() {
               Profile
             </a>
 
+            <a
+              href="/financial-advice"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Financial Advice
+            </a>
+
             <button
               type="button"
               onClick={() => {
@@ -73,6 +80,13 @@ export default function Example() {
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Sign up
+            </a>
+
+            <a
+              href="/financial-advice"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Financial Advice
             </a>
           </>}
 

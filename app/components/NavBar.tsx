f6fbb100a07ef1edb32650b9525d8e0dd40721aa
ozyegenl <lara.ozyegen@bahag.com>
2024-12-05 11:41:30 +0100

    email and download icons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -82,7 +82,7 @@ export default function Example() {
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
-              className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="relative rounded-full bg-white p-1 text-gray-500 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <span className="absolute -inset-1.5" />
               <span className="sr-only">View notifications</span>
